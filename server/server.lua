872eb42d2211d25fbd52b68f44435cd9c0634a6e
DuncanEll <120644682+DuncanEll2@users.noreply.github.com>
2024-06-20 21:24:52 +0100

    fixed description issue

--- server/server.lua
+++ server/server.lua
@@ -1,59 +1,85 @@
+RegisterServerEvent("supreme_oxitem:addItem")
+AddEventHandler(
+    "supreme_oxitem:addItem",
+    function(name, label2, weight, stack, close, allowarmed, description)
+        print(name, label2, weight, stack, close, allowarmed, description)
+        if IsPlayerAceAllowed(source, "oxitems") then
+            local src = source
+            local path = GetResourcePath("ox_inventory")
+            path = path:gsub("//", "/") .. "/data/items.lua"
 
-RegisterServerEvent('supreme_oxitem:addItem')
-AddEventHandler('supreme_oxitem:addItem', function(name, label2, weight, stack, close, allowarmed, description)
-	if IsPlayerAceAllowed(source, "oxitems") then
-	local src = source
-	local path = GetResourcePath("ox_inventory")
-	path = path:gsub('//', '/')..'/data/items.lua'
+            -- Open the file for reading
+            local file = io.open(path, "r")
+            local lines = {}
+            for line in file:lines() do
+                table.insert(lines, line)
+            end
+            table.remove(lines)
+            file:close()
+            file = io.open(path, "w")
+            file:write(table.concat(lines, "\n "))
 
-    -- Open the file for reading
-    local file = io.open(path, "r")
-    local lines = {}
-    for line in file:lines() do
-        table.insert(lines, line)
+            file:close()
+            -- Removes Last Line
+            Wait(1000)
+
+            file = io.open(path, "a+")
+            if not itemname then
+                label = "\n   ['" .. name .. "'] = {\n"
+            else
+                label = "\n\n-- " .. itemname .. "\n[]"
+            end
+            file:write(label)
+            file:write(
+                "     label = '" ..
+                    label2 ..
+                        "',\n     " ..
+                            "weight = " ..
+                                weight ..
+                                    ",\n     " ..
+                                        "stack = " ..
+                                            stack ..
+                                                ",\n     " ..
+                                                    "close = " ..
+                                                        close ..
+                                                            ",\n     " ..
+                                                                "allowArmed = '" ..
+                                                                    allowarmed ..
+                                                                        "',\n    " ..
+                                                                            "description = '" .. description .. "',\n"
+            )
+            file:write("},\n}")
+            file:close()
+        end
     end
-    table.remove(lines)
-    file:close()
-    file = io.open(path, "w")
-    file:write(table.concat(lines,"\n "))
-
-    file:close()
-    -- Removes Last Line
-	Wait(1000)
-
-	file = io.open(path, 'a+')
-	if not itemname then label = "\n   ['"..name.."'] = {\n"
-	else
-		label = '\n\n-- '..itemname.. '\n[]'
-	end
-	file:write(label)
-	file:write(
-		
-	"     label = '"..label2.."',\n     ".."weight = "..weight..",\n     ".."stack = "..stack..",\n     ".."close = "..close..",\n     ".."allowArmed = '"..allowarmed.."',\n    ".."description = '"..description.."',\n"
 )
-	file:write('},\n}')
-	file:close()
-end
-end)
 
-RegisterServerEvent('supreme_oxitem:addImage')
-AddEventHandler('supreme_oxitem:addImage', function(name, url)
-	if IsPlayerAceAllowed(source, "oxitems") then
-	PerformHttpRequest(url, function (errorCode, imageData, resultHeaders)
-		local path = GetResourcePath("ox_inventory")
-		path = path:gsub('//', '/')..'/web/images/'..name..'.png'
-		local f = assert(io.open(path, 'wb')) -- open in "binary" mode
-        f:write(imageData)
-        f:close()
-	  end)
-	end
-end)
-
-
-RegisterCommand('oxdevtools', function(source)
-    if IsPlayerAceAllowed(source, "oxitems") then
-        TriggerClientEvent("supreme_oxitems:openDevTools", source)
-	else
-		print('not allowed')
+RegisterServerEvent("supreme_oxitem:addImage")
+AddEventHandler(
+    "supreme_oxitem:addImage",
+    function(name, url)
+        if IsPlayerAceAllowed(source, "oxitems") then
+            PerformHttpRequest(
+                url,
+                function(errorCode, imageData, resultHeaders)
+                    local path = GetResourcePath("ox_inventory")
+                    path = path:gsub("//", "/") .. "/web/images/" .. name .. ".png"
+                    local f = assert(io.open(path, "wb")) -- open in "binary" mode
+                    f:write(imageData)
+                    f:close()
+                end
+            )
+        end
     end
-end)
+)
+
+RegisterCommand(
+    "oxdevtools",
+    function(source)
+        if IsPlayerAceAllowed(source, "oxitems") then
+            TriggerClientEvent("supreme_oxitems:openDevTools", source)
+        else
+           print("not allowed")
+        end
+    end
+)
